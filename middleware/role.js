d966dbaa2df12ff2e35e0a39b5075e6fc7c61893
priscilla irene <irenenangobipriscilla@gmail.com>
2025-03-16 17:33:55 +0300

    Update role.js

--- middleware/role.js
+++ middleware/role.js
@@ -1,11 +1,20 @@
 exports.authorize = async (req, res, next) => {
     try {
+         // Ensure req.user is defined
+      if (!req.user) {
+          return res.status(401).json({ message: 'Unauthorized' });
+      }
+        
       const role = req.user.role;
+              // Check if the role is 'admin' or 'chapterPresident'
       if (role !== 'admin' && role !== 'chapterPresident') {
         return res.status(403).json({ message: 'Forbidden' });
       }
+        
+      // Proceed to the next middleware if authorized
       next();
     } catch (error) {
+          // Handle unexpected errors
       res.status(500).json({ message: 'Internal Server Error' });
     }
   };
