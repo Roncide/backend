e5ec35402946dc733469666cced37cd3499447dc
priscilla irene <irenenangobipriscilla@gmail.com>
2025-03-16 17:27:32 +0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,14 +1,19 @@
+// db.js
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
+
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password : 'root',
-    database: 'cse'   
+    password: 'your_password',
+    database: 'your_database'
 });
 
-connection.connect(err=>{
-    if (err) throw err;
-    console.log('MySQL connected');
+db.connect(err => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database');
 });
 
-module.exports = connection
+module.exports = db;
