d26fdcc20cdb466904b9dee125a9cf655c8d8459
priscilla irene <irenenangobipriscilla@gmail.com>
2025-03-16 17:31:24 +0300

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,5 @@
 exports.handleError = async (err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error(err);//logging the error to the console
+    res.status(500).json({ message: 'Internal Server Error' });//sending a generic error response
   };
   
