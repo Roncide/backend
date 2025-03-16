ef35b3035bb77014f0ec974d3220d476455d6508
priscilla irene <irenenangobipriscilla@gmail.com>
2025-03-16 17:17:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const bodyPaser = require('body-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
+const.error=require('./middleware/error');//assuming error handleris in 'middleware/error.js'
 
 app.use(bodyParser.json());
 app.use(error.handleError);
 
 //Use routes
 app.use('/api',routes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
